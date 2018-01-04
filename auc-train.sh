c027211f07584fd429dcba86a134507f68e47fef
Liu Yuyang <lyuyang@gmail.com>
2018-01-04 19:13:43 +0800

    Fix mis-spelling of auc

--- auc-train.sh
+++ auc-train.sh
@@ -3,5 +3,5 @@ tail -n 891 data/train.csv | python gen_feature.py 1 > train.fea.csv
 tail -n 891 data/train.csv | awk -F"," '{ print $2 }' > train.label.csv
 tail -n 418 data/test.csv | python gen_feature.py 0 > test.fea.csv
 python train_n_predict.py train.fea.csv train.label.csv train.fea.csv > answer.label.csv
-paste answer.label.csv train.label.csv > train.lc.csv
+paste answer.label.csv train.label.csv > train.auc.csv
 rm answer.label.csv
