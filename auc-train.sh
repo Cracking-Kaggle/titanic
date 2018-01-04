0c34c09af584ed0cc3314175aa3c8eb83b91f008
Liu Yuyang <lyuyang@gmail.com>
2018-01-04 19:26:24 +0800

    Enhance scripts: run.sh accepts training method suffix

--- auc-train.sh
+++ auc-train.sh
@@ -2,6 +2,6 @@
 tail -n 891 data/train.csv | python gen_feature.py 1 > train.fea.csv
 tail -n 891 data/train.csv | awk -F"," '{ print $2 }' > train.label.csv
 tail -n 418 data/test.csv | python gen_feature.py 0 > test.fea.csv
-python train_n_predict.py train.fea.csv train.label.csv train.fea.csv > answer.label.csv
-paste answer.label.csv train.label.csv > train.auc.csv
-rm answer.label.csv
+python train_n_predict.py train.fea.csv train.label.csv train.fea.csv > answer.predict.csv
+paste answer.predict.csv train.label.csv > train.auc.csv; auc.sh train.auc.csv
+rm train.auc.csv train.fea.csv train.label.csv test.fea.csv answer.predict.csv
--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 tail -n 891 data/train.csv | python gen_feature.py 1 > train.fea.csv
 tail -n 891 data/train.csv | awk -F"," '{ print $2 }' > train.label.csv
 tail -n 418 data/test.csv | python gen_feature.py 0 > test.fea.csv
-python train_n_predict.py train.fea.csv train.label.csv test.fea.csv > answer.label.csv
-tail -n 418 data/test.csv | awk -F"," '{ print $1 }' > answer.id.csv; echo 'PassengerId,Survived' > answer.csv; paste -d "," answer.id.csv answer.label.csv >> answer.csv
-rm answer.label.csv answer.id.csv
+python train_n_predict.$1.py train.fea.csv train.label.csv test.fea.csv > answer.predict.csv
+tail -n 418 data/test.csv | awk -F"," '{ print $1 }' > answer.id.csv; echo 'PassengerId,Survived' > answer.csv; paste -d "," answer.id.csv answer.predict.csv >> answer.csv
+rm train.fea.csv train.label.csv test.fea.csv answer.predict.csv answer.id.csv
